ea973344a4503c64da01bee5d037e77475ed298c
nelispilkova <nelispilkova@gmail.com>
2017-03-18 15:44:18 +0200

    loginstyle

--- login.php
+++ login.php
@@ -13,13 +13,13 @@
             if (is_uploaded_file($fileOnServerName)) {
                 if (move_uploaded_file($fileOnServerName,
                     "./dir/$fileOriginalName")) {
-                    echo "Bravo, ti uspq! ";
+                    echo "Успешно качихте симката! ";
                 } else {
-                    echo "Tuka si grozen! Smeni!";
+                    echo "Нещо се обърка! Опитайте пак!";
                 }
             }
             else {
-                echo "Tuka si grozen! Smeni!";
+                echo "Нещо се обърка! Опитайте пак!";
             }
         }
 
@@ -60,35 +60,42 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="./New_project/miniMailProjekct/assets/css/loginStyle.css" type="text/css" />
     <title>Document</title>
 </head>
 <body>
 
 
-
+<div id="login">
     <form enctype='multipart/form-data' action="<?php $_SERVER['PHP_SELF']?>" method="post">
 
-
-        <label for="userName"> Име: </label>
-        <input type="text" name="userName" id="userName">
-        <br/>
-        <label for="userEmail">Email:</label>
-        <input type="email" name="userEmail" id="userEmail">
-        <br/>
-        <label for="img">Снимки: </label>
-        <input name="image" id="img" type="file"  accept="image/*" />
-        <input type='hidden' name='MAX_FILE_SIZE' value='8000000' />
-        <br/>
-        <label for="status">Семейно положение</label>
-        <select id="status" name="stat">
-            <option value="Щастливо сам/а">Щастливо сам/а</option>
-            <option value="За жалост стар/а">За жалост стар/а</option>
-            <option value="Бракуван/а">Бракуван/а</option>
-            <option value="Първа младост">Първа младост</option>
-        </select>
-        <br/>
+		<div class="pole">
+	        <label for="userName"> Име: </label>
+	        <input type="text" name="userName" id="userName">
+        </div>
+	    <div class="pole">
+	        <label for="userEmail">Email:</label>
+        	<input type="email" name="userEmail" id="userEmail">
+	    </div>
+	    <div class="pole">
+	        <label for="img">Снимки: </label>
+	        <input name="image" id="img" type="file"  accept="image/*" />
+	        <input type='hidden' name='MAX_FILE_SIZE' value='8000000' />
+        </div>
+        <div class="pole">
+	        <label for="status">Семейно положение</label>
+	        <select id="status" name="stat">
+	            <option value="Щастливо сам/а">Щастливо сам/а</option>
+	            <option value="За жалост стар/а">За жалост стар/а</option>
+	            <option value="Бракуван/а">Бракуван/а</option>
+	            <option value="Първа младост">Първа младост</option>
+	        </select>
+	        </div>
+        <div class="pole">
             <input type="submit" id="upload" name="submit" value="Запиши" >
+        </div>
     </form>
+</div>
 
 
 </body>
